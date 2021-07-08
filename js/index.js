2d4d15f392a3b97df194fb65b45325f97146da0a
NGFuture <78330887+NGFuture@users.noreply.github.com>
2021-07-08 12:35:17 -0700

    homework 6-2- fetch

--- js/index.js
+++ js/index.js
@@ -1,29 +1,15 @@
 const today=new Date()
-console.log(today)
-
 const thisYear=today.getFullYear();
-console.log(thisYear)
-
 const footer=document.querySelector('footer')
-console.log(footer)
-
 const copyright = document.createElement("p")
-console.log(copyright)
-
 copyright.innerHTML=`<small>&copy; ${thisYear} Natalia Grigoryeva</small>`
-console.log(copyright)
-
 footer.appendChild(copyright)
 
-const skills = ["Coding JavaScript","WebDesign",'Photography'];
-console.log(skills)
-
+const skills = ["Coding JavaScript","WebDesign",'Photography', "Time management"];
 
 const skillsSelection=document.getElementById('skills')
-console.log(skillsSelection)
 if (skillsSelection !== null) {
     const skillsList=skillsSelection.querySelector('ul')
-    console.log(skillsList)
     
     for(i=0;i<skills.length;i++){
         
@@ -32,7 +18,6 @@ if (skillsSelection !== null) {
         skillsList.appendChild(skill)
     
     }
-    console.log(skillsList)
 }
 
 const messageForm=document.getElementsByName('leave_message')[0]
@@ -43,7 +28,7 @@ messageForm.addEventListener('submit', (e) =>{
     const email=e.target.email.value;
     const message=e.target.message.value;
     //console.log (name, email, message');
-    console.log(`Name: ${name}, email: ${email}, message: ${message}`);
+    // console.log(`Name: ${name}, email: ${email}, message: ${message}`);
 
     const messageSection=document.getElementById('messages');
     const messageList=messageSection.getElementsByTagName('ul')[0];
@@ -51,64 +36,58 @@ messageForm.addEventListener('submit', (e) =>{
     newMessage.innerHTML=`
         <a href="mailto:${email}"> ${name} </a>
         <span> wrote: ${message}</span>`
-    // messageList.appendChild(newMessage) 
-    console.log(newMessage)
-
     const removeButton=document.createElement('button');
     removeButton.innerText='remove';
     removeButton.type='button';
     removeButton.addEventListener('click', (event)=>{
         const entry=event.target.parentNode //find the button's parent element using DOM Traversal (hint: parentNode property) and store it in a variable named entry
         entry.remove();
-
     })
 
     newMessage.appendChild(removeButton);
     messageList.appendChild(newMessage)
 
     messageForm.reset();
-
 })
 
-//Request to GITHUB
-const githubRequest = new XMLHttpRequest();
-const GITHUB_USERNAME = 'NGFuture';
-githubRequest.open("GET", `https://api.github.com/users/${GITHUB_USERNAME}/repos`);
-// form the request detail
-githubRequest.send();
+//Request to GITHUB by HMLHTTPrequest
+// const githubRequest = new XMLHttpRequest();
+// // const GITHUB_USERNAME = 'NGFuture';
+// githubRequest.open("GET", `https://api.github.com/users/${GITHUB_USERNAME}/repos`);
+// // form the request detail
+// githubRequest.send();
 
-// when we get data, read it
-
-// variant 1
-// githubRequest.addEventListener('load', (e) => {
-//     const resp = e.target.response;
-
-//     const data = JSON.parse(resp);
-
-//     console.log(data)
-// })
-
-//variant 2
-githubRequest.onload = function() {  
-    const repositories = JSON.parse(this.response);
+// githubRequest.onload = function() {  
+//     const repositories = JSON.parse(this.response);
     
-    const projectSection = document.getElementById('projects');
-    const projectList = projectSection.querySelector('ul');
+//     const projectSection = document.getElementById('projects');
+//     const projectList = projectSection.querySelector('ul');
 
-    repositories.forEach(repo =>{
-        const project = document.createElement('li');
-        console.log(repo);
-        
-        //project.innerText = repo.name;
+//     repositories.forEach(repo =>{
+//         const project = document.createElement('li');
+//         console.log(repo);
 
-        project.innerHTML=`
-        <a href="${repo.html_url}"> Project ${repo.name} </a>`
+//         project.innerHTML=`
+//         <a href="${repo.html_url}"> Project ${repo.name} </a>`
 
-        projectList.appendChild(project);
+//         projectList.appendChild(project);
 
-    })
+//     })
     
-    
-    
-    console.log(repositories);
-}
+ 
+// }
+
+//Request to GITHUB by Fetch 
+const GITHUB_USERNAME = 'NGFuture'
+fetch(`https://api.github.com/users/${GITHUB_USERNAME}/repos`)
+    .then(response =>response.json())
+    .then(response => {  
+            const projectSection = document.getElementById('projects');
+            const projectList = projectSection.querySelector('ul');
+                 response.forEach(repo =>{
+                    const project = document.createElement('li');   
+                    project.innerHTML=`<a href="${repo.html_url}"> Project ${repo.name} </a>`           
+                    projectList.appendChild(project);
+                    })
+            }
+    )
